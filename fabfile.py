9e7419ba47716b1ac251a178dfad2c3ce8c253d0
Anthony Lapenna <lapenna.anthony@gmail.com>
2018-08-02 09:44:09 +0200

    feat(fabfile): update fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,19 +2,19 @@ from fabric.api import task, local
 
 @task
 def down():
-    local('docker-compose down')
+    local('/usr/local/bin/docker-compose down')
     local('rm -rf /tmp/manager_run')
 
 @task
 def up():
-    local('docker-compose up -d manager1 manager2 worker1 worker2')
-    local('docker-compose exec -T manager1 docker swarm init')
-    token_worker = local('docker-compose exec -T manager1 docker swarm join-token -q worker', capture=True)
-    token_manager = local('docker-compose exec -T manager1 docker swarm join-token -q manager', capture=True)
-    local('docker-compose exec -T manager2 docker swarm join --token {} manager1:2377'.format(token_manager))
-    local('docker-compose exec -T worker1 docker swarm join --token {} manager1:2377'.format(token_worker))
-    local('docker-compose exec -T worker2 docker swarm join --token {} manager1:2377'.format(token_worker))
-    local('docker-compose up -d proxy portainer')
+    local('/usr/local/bin/docker-compose up -d manager1 manager2 worker1 worker2')
+    local('/usr/local/bin/docker-compose exec -T manager1 docker swarm init')
+    token_worker = local('/usr/local/bin/docker-compose exec -T manager1 docker swarm join-token -q worker', capture=True)
+    token_manager = local('/usr/local/bin/docker-compose exec -T manager1 docker swarm join-token -q manager', capture=True)
+    local('/usr/local/bin/docker-compose exec -T manager2 docker swarm join --token {} manager1:2377'.format(token_manager))
+    local('/usr/local/bin/docker-compose exec -T worker1 docker swarm join --token {} manager1:2377'.format(token_worker))
+    local('/usr/local/bin/docker-compose exec -T worker2 docker swarm join --token {} manager1:2377'.format(token_worker))
+    local('/usr/local/bin/docker-compose up -d proxy portainer')
 
 @task
 def demo():
